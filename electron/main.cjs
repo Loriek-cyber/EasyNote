202d16391e08635792e883a8c1f9be575b67f3c2
Arjel Buzi <arjel.buzi1@gmail.com>
2025-10-24 22:19:44 +0200

    removing every ai slot

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,18 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const DatabaseManager = require('../src/model/db.cjs');
-const { DocDAO } = require('../src/model/DAO/docdao.js');
 
-const dbManager = new DatabaseManager();
-let docDAO;
-
-async function initializeDatabase() {
-    const dbPath = path.join(app.getPath('userData'), 'easymeet.sqlite');
-    await dbManager.connect(dbPath);
-    docDAO = new DocDAO(dbManager); // Pass the manager, not the raw db
-    await docDAO.createTable();
-    console.log('Database initialized and table is ready.');
-}
 
 function createWindow() {
    const win = new BrowserWindow({
@@ -28,24 +15,8 @@ function createWindow() {
   win.loadURL('http://localhost:5173');
 }
 
-function setupDatabaseIPC() {
-    // Document-specific handlers
-    ipcMain.handle('doc:create', (_, doc) => docDAO.create(doc));
-    ipcMain.handle('doc:getAll', () => docDAO.getAll());
-    ipcMain.handle('doc:getById', (_, id) => docDAO.getById(id));
-    ipcMain.handle('doc:update', (_, id, doc) => docDAO.update(id, doc));
-    ipcMain.handle('doc:delete', (_, id) => docDAO.delete(id));
-    ipcMain.handle('doc:findByPath', (_, path) => docDAO.findByPath(path));
-
-    // General DB connection status
-    ipcMain.handle('db:isConnected', () => dbManager.isConnected());
-}
-
 app.whenReady().then(async () => {
-    await initializeDatabase();
-    setupDatabaseIPC();
     createWindow();
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,46 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Create safe database API for renderer
 contextBridge.exposeInMainWorld('database', {
-    run: async (sql, params) => {
-        try {
-            return await ipcRenderer.invoke('db:run', sql, params);
-        } catch (err) {
-            console.error('Database run error:', err);
-            throw err;
-        }
-    },
-    get: async (sql, params) => {
-        try {
-            return await ipcRenderer.invoke('db:get', sql, params);
-        } catch (err) {
-            console.error('Database get error:', err);
-            throw err;
-        }
-    },
-    all: async (sql, params) => {
-        try {
-            return await ipcRenderer.invoke('db:all', sql, params);
-        } catch (err) {
-            console.error('Database all error:', err);
-            throw err;
-        }
-    },
-    switchDB: async (dbPath) => {
-        try {
-            return await ipcRenderer.invoke('db:switch', dbPath);
-        } catch (err) {
-            console.error('Database switch error:', err);
-            throw err;
-        }
-    },
-    isok: async () => {
-        try {
-            return await ipcRenderer.invoke('db:isConnected');
-        } catch (err) {
-            console.error('Database connection check error:', err);
-            return false;
-        }
-    }
+    
 });
 
 
