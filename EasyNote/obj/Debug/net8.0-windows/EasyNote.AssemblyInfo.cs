f0002cfa9780ac18c8f84febf11a58062f73aa34
Arjel Buzi <arjel.buzi1@gmail.com>
2025-11-19 19:06:18 +0100

    nope

--- EasyNote/obj/Debug/net8.0-windows/EasyNote.AssemblyInfo.cs
+++ EasyNote/obj/Debug/net8.0-windows/EasyNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e13294bda5fd91a7fb07a84a5a4efc4a9e1c24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3343f1f8d273dc9fbc770c86a724a514a63acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
